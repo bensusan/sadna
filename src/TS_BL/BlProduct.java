920af64b6286972f1f47d9b916b43ca5d37e75d3
amitkapl <38068934+amitkapl@users.noreply.github.com>
2018-04-18 18:32:36 +0300

    Edit mistakes

--- src/TS_BL/BlProduct.java
+++ src/TS_BL/BlProduct.java
@@ -5,13 +5,9 @@ import TS_SharedClasses.*;
 public class BlProduct {
 
 	public static boolean purchase(Product p, Guest g, int price, int amount) {
-<<<<<<< HEAD
-		return BlMain.purchase(p.getPurchasePolicy(), g, price, amount) && BlMain.buyProduct(p.getStore(), p, amount) && BlMain.payToStore(p.getStore(), price);
-=======
 		PurchasePolicy policy = p.getStore().getStorePolicy();
 		if(policy == null)
 			policy = p.getPurchasePolicy();
 		return BlMain.purchase(policy , g, price, amount) && BlMain.buyProduct(p.getStore(), p, amount) && BlMain.payToStore(p.getStore(), price);
->>>>>>> 3f69e1fd2e7b78391fde25f8d0372d2c0c929ae4
 	}
 }

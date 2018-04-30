427d68f2ee1a79206db133b27ac6623bd0baffd6
or <or512749@gmail.com>
2018-04-30 16:15:20 +0300

    correct  regression tests

--- src/UnitTests/storeOwnerTests.java
+++ src/UnitTests/storeOwnerTests.java
@@ -1,4 +1,4 @@
-package tests;
+package UnitTests;
 
 import static org.junit.Assert.*;
 
---
+++ src/tests/regressionTests/GuestRegTests.java
@@ -0,0 +1,49 @@
+package tests.regressionTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GuestRegTests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testAddProductToCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveProductFromCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPuchaseCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPruchaseProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSignUp() {
+		fail("Not yet implemented");
+	}
+
+}

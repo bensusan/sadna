a44abcf3991da43fab6444c5eca87dac7a7f49d8
or <or512749@gmail.com>
2018-05-28 21:36:17 +0300

    dal signatures

--- src/main/java/TS_DAL/DAL.java
+++ src/main/java/TS_DAL/DAL.java
@@ -1,6 +1,11 @@
 package TS_DAL;
 
 import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import TS_SharedClasses.*;
 
 public class DAL {
 	//TINYINT used for boolean, 0-false;1-true
@@ -63,6 +68,7 @@ public class DAL {
 					+ " name VARCHAR(50),"
 					+ " price INTEGER,"
 					+ " grading INTEGER ,"
+					+" category VARCHAR(50),"
 					+ "PRIMARY KEY (productId));";
 			stmt.executeUpdate(sql);
 			sql = "CREATE TABLE StoreManagers(username VARCHAR(50) REFERENCES Subscribers(username),"
@@ -118,7 +124,173 @@ public class DAL {
 	public static DAL getInstance(){
 		return dal;
 	}
+	public List<Subscriber> allSubscribers() throws Exception
+	{
+		String query ="SELECT * FROM Subscribers";
+		Connection c=getConnection();
+		Statement statement=c.createStatement();
+		ResultSet rs=statement.executeQuery(query);
+		List<Subscriber>ans=new LinkedList<Subscriber>();
+		while(rs.next())
+		{
+			List<Purchase> myPurchase=getMyPurchase(rs.getString("username"));
+			List<StoreManager>managers=getSubscriberManagers(rs.getString("username"));
+			List<StoreOwner>owners=getSubscriberOwners(rs.getString("username"));
+			Subscriber s=new Subscriber(
+					rs.getString("username"),
+					rs.getString("password"),
+					rs.getString("fullname"),
+					rs.getString("address"),
+					rs.getString("phone"),
+					rs.getString("credidCardNumber"),
+					myPurchase,
+					managers,
+					owners);
+			ans.add(s);
+		}
+		return ans;
+		
+	}
 
+	public List<StoreOwner> getSubscriberOwners(String username) throws Exception {
+		String query ="SELECT * FROM storeowners WHERE username="+username+";";
+		Connection c=getConnection();
+		Statement statement=c.createStatement();
+		ResultSet rs=statement.executeQuery(query);
+		List<StoreOwner>ans=new LinkedList<StoreOwner>();
+		while(rs.next())
+		{
+			StoreOwner so=new StoreOwner(getStoreByStoreId(rs.getString("storeId")));
+			ans.add(so);
+		}
+		return ans;
+	}
+	
+	public Store getStoreByStoreId(String storeId) {
+		// TODO return Store by store id
+		return null;
+	}
+	
+	public  List<StoreManager> getSubscriberManagers(String username) {
+		// TODO return all StoreManagers of Subscriber with given username
+		return null;
+	}
+	
+	public  List<Purchase> getMyPurchase(String username) {
+		// TODO return all purchase of Subscriber with given username
+		return null;
+	}
+	
+	public boolean isSubscriberExist(String username){
+		// TODO return all purchase of Subscriber with given username
+		return false;
+	}
+	
+	public boolean isAdmin(String username)
+	{
+		// TODO return all purchase of Subscriber with given username
+		return false;
+	}
+
+	public void removeSubscriber(){
+		// TODO return all purchase of Subscriber with given username
+	}
+
+	public List<Purchase> getStorePurchase(Store store){
+		return null;
+	}
+
+	public void addStoreOwner(Subscriber s,StoreOwner so){	
+	}
+
+	public void addPurchaseToHistory(Subscriber sub, Purchase p){
+		
+	}
+	
+	public void addOwner(Subscriber sub, StoreOwner owner){
+		
+	}
+	
+	public void addManager(Subscriber sub, StoreManager manager){
+		
+	}
+
+	public void deleteOwner(Subscriber sub, StoreOwner owner){
+		
+	}
+
+	public void deleteManager(Subscriber sub, StoreManager manager){
+		
+	}
+	
+	public boolean checkInStock(Product p, int amount){
+		return false;
+		}
+
+	public Store getProductStore(Product p){
+		return null;
+	}
+
+	public Map<Store,Integer> getStoreProduct(Store store){
+		return null;
+	}
+
+	public void stockUpdate(Product p, int amount,Store store){
+		
+	}
+
+	public void updategetMoneyEarned(Store s, int newMoneyEarend){
+		
+	}
+
+	public List<Category>getAllCategory(){
+		return null;
+		
+	}
+	
+	public Category getCategory(String categoryName){
+		return null;
+		
+	}
+
+	public void addProductToStore(Store s, Product product, int amount,String category){
+		
+	}
+
+	public void deleteProductFromStore(Store s, Product product){
+		
+	}
+
+	public void updateProductDetails(Store s, Product oldProduct, Product newProduct, int amount,String newProductCategory)
+	{
+		
+	}
+
+	public void addNewStoreOwner(Store s, Subscriber owner){
+		
+	}
+
+	public void addNewManager(Store s, Subscriber newMan){
+		
+	}
+
+	public void updateCloseStore(Store s){
+		
+	}
+
+	public void upadteOpenStore(Store s){
+		
+	}
+	
+	public void addSubscriber(Subscriber s){
+		
+	}
+
+	public Subscriber getSubscriber(String username, String password){
+		return null;
+	}
+
+	
 	protected static Connection getConnection() throws Exception {
 		String driver = "com.mysql.cj.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
